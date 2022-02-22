b1ceffd7fdad011c97f750f1c13936d692469e5e
Minnig Agustin <minnigagustin@gmail.com>
2022-02-22 15:00:51 -0300

    estilos

--- pantallas/Historial.js
+++ pantallas/Historial.js
@@ -5,6 +5,7 @@ import {
   Dimensions,
   TouchableOpacity,
   ScrollView,
+  Modal,
 } from "react-native";
 
 import { Text } from "react-native-paper";
@@ -15,6 +16,7 @@ import tailwind from "tailwind-rn";
 const width = Dimensions.get("window").width;
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   const [data, setData] = useState([]);
 
   const readData = async () => {
@@ -35,6 +37,54 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View>
+          <View
+            style={tailwind(
+              "h-full items-center justify-center bg-gray-500 p-12 pt-40"
+            )}
+          >
+            <View
+              style={tailwind("bg-white w-full p-6 rounded-lg items-center")}
+            >
+              <MaterialCommunityIcons
+                name={"bookmark-check"}
+                size={58}
+                color="#900"
+              />
+
+              <Text style={tailwind("text-gray-800 text-xl font-medium mt-4")}>
+                ¡Datos enviados!
+              </Text>
+
+              <Text style={tailwind("text-gray-600 text-center mt-2 w-56")}>
+                ¡Revisa tu casilla de email!
+              </Text>
+
+              <TouchableOpacity
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+                style={tailwind(
+                  "bg-indigo-600 w-full py-2 items-center rounded-md mt-6"
+                )}
+              >
+                <Text style={tailwind("text-white font-medium")}>
+                  Volver atras!
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
       <View
         style={{
           width: Dimensions.get("window").width,
@@ -58,7 +108,7 @@ export default function App() {
             <View
               key={i}
               style={tailwind(
-                "justify-center items-center p-4 w-4/5 rounded bg-green-500 my-2"
+                "justify-center items-center p-4 w-4/5 rounded bg-green-400 my-2"
               )}
             >
               <Text style={{ fontWeight: "bold" }}>{item.cuig}</Text>
@@ -66,6 +116,20 @@ export default function App() {
               <Text>{item.numero}</Text>
             </View>
           ))}
+          {data.length != 0 ? <View style={{width: width, alignItems: 'center'}}>
+          <TouchableOpacity
+            onPress={() => {
+              setModalVisible(!modalVisible);
+            }}
+            style={tailwind(
+              "justify-center items-center p-4 w-4/5 rounded bg-green-600 my-2"
+            )}
+          >
+            <Text style={tailwind("text-lg text-white")}>
+              <MaterialCommunityIcons name={"email"} size={21} /> Enviar{" "}
+              <MaterialCommunityIcons name={"email"} size={21} />
+            </Text>
+          </TouchableOpacity>
           <TouchableOpacity
             onPress={() => {
               AsyncStorage.setItem("vacas", JSON.stringify([]));
@@ -75,7 +139,7 @@ export default function App() {
             )}
           >
             <Text style={tailwind("text-lg text-white")}>Limpiar</Text>
-          </TouchableOpacity>
+          </TouchableOpacity></View> : null}
         </ScrollView>
       </View>
     </View>
