10bfe4c992e04bb7efd9aa97b315a00d1aa24a18
Minnig Agustin <minnigagustin@gmail.com>
2022-02-23 15:01:33 -0300

    PROBANDO EXCEL TODAVIA NO FUNCIONA

--- pantallas/Historial.js
+++ pantallas/Historial.js
@@ -13,6 +13,13 @@ import { Text } from "react-native-paper";
 import { MaterialCommunityIcons } from "react-native-vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import tailwind from "tailwind-rn";
+// TENGO QUE CAMBIAR ESTE CODIGO!
+// Required to save to cache 
+import * as FileSystem from 'expo-file-system';
+// ExcelJS
+import ExcelJS from 'exceljs';
+// Share excel via share dialog
+import * as Sharing from 'expo-sharing';
 
 const width = Dimensions.get("window").width;
 
@@ -35,6 +42,69 @@ export default function App() {
   useEffect(() => {
     readStorage();
   });
+
+  const generateShareableExcel = async () => {
+    const now = new Date();
+    const fileName = 'YourFilename.xlsx';
+    const fileUri = FileSystem.cacheDirectory + fileName;
+    return new Promise((resolve, reject) => {
+        const workbook = new ExcelJS.Workbook();
+        workbook.creator = 'Me';
+        workbook.created = now;
+        workbook.modified = now;
+        // Add a sheet to work on
+        const worksheet = workbook.addWorksheet('My Sheet', {});
+        // Just some columns as used on ExcelJS Readme
+        worksheet.columns = [
+            { header: 'Id', key: 'id', width: 10 },
+            { header: 'Name', key: 'name', width: 32 },
+            { header: 'D.O.B.', key: 'dob', width: 10, }
+        ];
+        // Add some test data
+        worksheet.addRow({ id: 1, name: 'John Doe', dob: new Date(1970, 1, 1) });
+        worksheet.addRow({ id: 2, name: 'Jane Doe', dob: new Date(1969, 2, 3) });
+        // Test styling
+        // Style first row
+        worksheet.getRow(1).font = {
+            name: 'Comic Sans MS', family: 4, size: 16, underline: 'double', bold: true
+        };
+        // Style second column
+        worksheet.eachRow((row, rowNumber) => {
+            row.getCell(2).font = {
+                name: 'Arial Black',
+                color: { argb: 'FF00FF00' },
+                family: 2,
+                size: 14,
+                bold: true
+            };
+        });
+        // Write to file
+        workbook.xlsx.writeBuffer().then((buffer) => {
+            // Do this to use base64 encoding
+            const nodeBuffer = NodeBuffer.from(buffer);
+            const bufferStr = nodeBuffer.toString('base64');
+            FileSystem.writeAsStringAsync(fileUri, bufferStr, {
+                encoding: FileSystem.EncodingType.Base64
+            }).then(() => {
+                resolve(fileUri);
+            });
+        });
+    });
+};
+const shareExcel = async () => {
+    const shareableExcelUri = await generateShareableExcel();
+    Sharing.shareAsync(shareableExcelUri, {
+        mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        dialogTitle: 'Your dialog title here',
+        UTI: 'com.microsoft.excel.xlsx' // iOS
+    }).catch(error => {
+        console.error('Error', error);
+    }).then(() => {
+        console.log('Return from sharing dialog');
+    });
+};
+
+
 
   return (
     <View style={styles.container}>
@@ -126,9 +196,9 @@ export default function App() {
           ))}
           {animals.length != 0 ? <View style={{ width: width, alignItems: 'center' }}>
             <TouchableOpacity
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}
+              onPress={
+                shareExcel
+              }
               style={tailwind(
                 "justify-center items-center p-4 w-4/5 rounded bg-green-600 my-2"
               )}
